081ef1edef084405beff17a0d370e4d9a942605d
OmoBolarinwaye <ajayibcd@gmail.com>
2020-07-21 09:49:18 +0100

    move sign up and sign in

--- Assets/script/custom.js
+++ Assets/script/custom.js
@@ -21,35 +21,4 @@
 
 
 
-/* the function below preveny default submission of form data and run our custom script
-* @param {Event}, @param{HTMLFormControlsCollection}
-* @return {void}, @return{object}
-*/
-//function that checks for validity
-const isValidElement = element => {
-   return element.name && element.value;
- };
 
-
-const formToJSON = elements => [].reduce.call(elements, (data, element) => {
-    // Make sure the element has the required properties.
-   if (isValidElement(element)) {
-      data[element.name] = element.value;
-   }
-   return data;
- }, {});
-
- const handleFormSubmit = event => {
-   // Stop the form from submitting since we’re handling that with AJAX.
-   event.preventDefault();
-   // Call our function to get the form data.
-   const data = formToJSON(form.elements);
-   // Demo only: print the form data onscreen as a formatted JSON object.
-   const dataContainer = document.getElementsByClassName('result-display')[0];
-   // Use `JSON.stringify()` to make the output valid, human-readable JSON.
-   dataContainer.textContent = JSON.stringify(data, null, "  ");
-   // ...this is where we’d actually do something with the form data...
- };
-
- const form = document.getElementsByClassName('form')[0];
- form.addEventListener('submit', handleFormSubmit);
--- Assets/script/fetchApi.js
+++ Assets/script/fetchApi.js
@@ -1,3 +1,5 @@
+//fetch api code1
+
 const kawuraform = document.getElementsByClassName('form');
 kawuraform.addEventListener('submit', function(e){
    e.preventDefault();
@@ -18,7 +20,7 @@ kawuraform.addEventListener('submit', function(e){
 })
 
 
-
+//fetch api code2
 const kawuraForm = document.getElementById('myForm');
 kawuraForm.addEventListener('submit', function(e) {
    e.preventDefault();
@@ -33,8 +35,9 @@ kawuraForm.addEventListener('submit', function(e) {
    }
    return data;
    }, {});
-   const formData = formToJSON(form.elements);
-   console.log(JSON.stringify(formData));
+   const formdata = formToJSON(form.elements);
+   const formData = JSON.stringify(formdata);
+   console.log(formData);
 
    const url ='https://kawuraweb.herokuapp.com/signin';
    fetch(url,{
@@ -49,3 +52,42 @@ kawuraForm.addEventListener('submit', function(e) {
    })
   
 });
+
+
+//Real gee on converting form input to json
+
+/* the function below preveny default submission of form data and run our custom script
+* @param {Event}, @param{HTMLFormControlsCollection}
+* @return {void}, @return{object}
+*/
+//function that checks for validity
+const isValidElement = element => {
+   return element.name && element.value;
+ };
+
+
+const formToJSON = elements => [].reduce.call(elements, (data, element) => {
+    // Make sure the element has the required properties.
+   if (isValidElement(element)) {
+      data[element.name] = element.value;
+   }
+   return data;
+ }, {});
+
+ const handleFormSubmit = event => {
+   // Stop the form from submitting since we’re handling that with AJAX.
+   event.preventDefault();
+   // Call our function to get the form data.
+   const data = formToJSON(form.elements);
+   // Demo only: print the form data onscreen as a formatted JSON object.
+   const dataContainer = document.getElementsByClassName('result-display')[0];
+   // Use `JSON.stringify()` to make the output valid, human-readable JSON.
+   dataContainer.textContent = JSON.stringify(data, null, "  ");
+   // ...this is where we’d actually do something with the form data...
+ };
+
+ const form = document.getElementsByClassName('form')[0];
+ form.addEventListener('submit', handleFormSubmit);
+
+
+
